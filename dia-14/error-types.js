90e6876bf7811a8558468b4b3cde43d6935759d4
Isabella <isacammerer@gmail.com>
2022-08-13 16:05:50 -0300

    Realização dos exercícios dia 14

---
+++ dia-14/error-types.js
@@ -0,0 +1,17 @@
+//ReferenceError - aparece se uma variável executada naõ foi declarada
+let firstName = 'Isabella'
+let fullName = firstName + ' ' + lastName
+console.log(fullName)
+//ReferenceError: lastName is not defined
+
+//SyntaxError - erro de syntax
+let square = 2 x 2
+console.log(square)
+
+console.log('Hello, world")
+//SyntaxError: Unexpected identifier
+
+//TypeError - erro de digitação
+let num = 10
+console.log(num.toLowerCase())
+//Uncaught TypeError: num.toLowerCase is not a function
